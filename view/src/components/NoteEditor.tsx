95a559caadaa58c17d68cb4f3dcdccbaa70ec7ca
gamabuntat <schoshoev@yandex.ru>
2023-03-30 20:35:27 +0800

    style prettier

--- view/src/components/NoteEditor.tsx
+++ view/src/components/NoteEditor.tsx
@@ -3,24 +3,25 @@ import React, { FunctionComponent, useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 export interface NoteEditorProps {
-    text: string
-    parentClickCallback?: () => void
-    changeTextCallback?: (content: string) => void
+    text: string;
+    parentClickCallback?: () => void;
+    changeTextCallback?: (content: string) => void;
 }
 
-export const NoteEditor: FunctionComponent<NoteEditorProps> = (noteEditorProps: NoteEditorProps) => {
+export const NoteEditor: FunctionComponent<NoteEditorProps> = (
+    noteEditorProps: NoteEditorProps,
+) => {
     const initialEdiorState: EditorState = EditorState.createWithContent(
-        ContentState.createFromText(noteEditorProps.text));
+        ContentState.createFromText(noteEditorProps.text),
+    );
     const _initialEdiorState = EditorState.moveFocusToEnd(initialEdiorState);
 
-    const [editorState, changeEditorState] = useState(_initialEdiorState)
-    
+    const [editorState, changeEditorState] = useState(_initialEdiorState);
+
     const handleChange = (e: EditorState) => {
         changeEditorState(e);
-        noteEditorProps.changeTextCallback?.(
-            editorState.getCurrentContent().getPlainText()
-        )
-    }
+        noteEditorProps.changeTextCallback?.(editorState.getCurrentContent().getPlainText());
+    };
 
     const onTabHandling = (e: React.KeyboardEvent<{}>) => {
         e.preventDefault();
@@ -28,26 +29,26 @@ export const NoteEditor: FunctionComponent<NoteEditorProps> = (noteEditorProps: 
         let newContentState = Modifier.replaceText(
             currentState.getCurrentContent(),
             currentState.getSelection(),
-            '\t'
+            '\t',
         );
-        changeEditorState(EditorState.push(
-            currentState, newContentState, 'insert-characters'))
-    }
+        changeEditorState(EditorState.push(currentState, newContentState, 'insert-characters'));
+    };
 
     const handleExitEditor = () => {
         noteEditorProps.parentClickCallback?.();
-    }
+    };
 
-    return(
+    return (
         <div onBlur={handleExitEditor}>
-            <Editor 
-                editorState={ editorState } 
-                onChange={ e => handleChange(e) }
-                onTab={e => onTabHandling(e)}
-            /> 
-            <hr/>
-            <Button onClick={handleExitEditor} 
-                size="sm">Save</Button>
+            <Editor
+                editorState={editorState}
+                onChange={(e) => handleChange(e)}
+                onTab={(e) => onTabHandling(e)}
+            />
+            <hr />
+            <Button onClick={handleExitEditor} size="sm">
+                Save
+            </Button>
         </div>
-    )
-}
+    );
+};
