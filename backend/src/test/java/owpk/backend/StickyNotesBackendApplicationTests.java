4b11a65c33b8071aac54a11d397709302c713fe1
owpk <voyzvz@gmail.com>
2023-03-30 16:42:22 +0800

    initial

---
+++ backend/src/test/java/owpk/backend/StickyNotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package owpk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickyNotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
